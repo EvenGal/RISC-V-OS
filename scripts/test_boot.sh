4d089af964c41400caf3f71a648da5778e2e2198
Even-Galasen_AvinorAS <even.galaasen@hotmail.com>
2025-03-23 22:57:45 +0100

    fix test

--- scripts/test_boot.sh
+++ scripts/test_boot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 set -e
 
 make clean && make
@@ -7,7 +7,7 @@ timeout 5 qemu-system-riscv64 \
   -machine virt -nographic -bios none -kernel build/kernel.elf \
   | tee qemu_output.txt
 
-grep -q "Hello" qemu_output.txt && echo "Boot test passed" || {
+grep -q "> " qemu_output.txt && echo "Boot test passed" || {
     echo "Boot test failed"
     exit 1
 }
